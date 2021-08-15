b430ac9c755cbd3ae314ff91180327ee18fd0097
RUiNtheExtinct <deepkarma001@gmail.com>
2021-08-15 11:16:57 +0530

    problem 47

--- python/p047.py
+++ python/p047.py
@@ -4,6 +4,7 @@
 # import collections as coll
 import sys
 import math as mt
+
 # import random as rd
 # import bisect as bi
 # import functions as fn
@@ -40,8 +41,22 @@ mod = 1000000007
 ######## ? CODE STARTS FROM HERE ########
 
 # for _ in range(uno()):
-n = uno()
-
+n, k = tres()
+a = [0 for i in range(n + k + 1)]
+for i in range(2, n + k + 1):
+    if a[i] == 0:
+        for j in range(i, n + k + 1, i):
+            a[j] += 1
+for i in range(n + 1):
+    b = False
+    for j in range(k):
+        if a[i + j] != k:
+            break
+        if j == k - 1:
+            b = True
+    if b:
+        print(i)
+        break  # ? Comment this line for HackerRank Solution.
 
 
 # End Time
